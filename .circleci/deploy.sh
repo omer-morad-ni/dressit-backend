979554729803b01b1197adc691f8cb5f785f4608
Omer Morad <omer.morad@naturalint.com>
2020-05-21 08:19:23 +0300

    Improved Pipeline

--- .circleci/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker pull omerico/dressit:latest
-docker stop dressit
-docker rm dressit
-docker rmi omerico/dressit:latest
-docker tag omerico/dressit:latest omerico/dressit:current
-docker run -d --name dressit omerico/dressit:latest
